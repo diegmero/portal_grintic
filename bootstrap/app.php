667a055ee831c300479d91c54372d44641eaabfb
diegmero <diegmero12@gmail.com>
2026-01-18 00:10:19 -0500

    feat: add Sentry error monitoring integration
    - Install sentry/sentry-laravel v4.20
    - Configure Integration::handles() in bootstrap/app.php
    - Add sentry_logs channel to logging.php
    - Add Sentry DSN and config to .env
    - Enable performance monitoring and PII collection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,6 +29,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        // Sentry error monitoring
+        Integration::handles($exceptions);
+        
         $exceptions->respond(function (\Symfony\Component\HttpFoundation\Response $response) {
              $status = $response->getStatusCode();
              if (in_array($status, [403, 404, 500, 503])) {
@@ -38,3 +42,4 @@ return Application::configure(basePath: dirname(__DIR__))
              return $response;
         });
     })->create();
+
